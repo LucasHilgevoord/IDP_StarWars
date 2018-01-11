f4fcdf7bf749d7814319c8502826e3f45ed7877f
mrSmellyfeet100 <boraoxal@gmail.com>
2018-01-11 09:38:09 +0100

    added Player forward movement

---
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	[SerializeField]float movementSpeed = 10;
+
+	Transform obT;
+
+	void Awake()
+	{
+		obT = transform;
+	}
+
+	void Update () 
+	{
+		Thrust ();
+	}
+
+	void Thrust()
+	{
+		if(Input.GetAxis("Vertical") > 0)
+		obT.position += obT.forward * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+	}
+
+}
