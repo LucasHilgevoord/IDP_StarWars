b3ea05e01b26ebaee4c86522f51d144728d0dd6f
Lucas Hilgevoord <lucashilgevoord@hotmail.com>
2018-01-15 11:42:19 +0100

    Walker Head Rotation Fix

---
+++ Assets/Scripts/Walker/Walker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walker : MonoBehaviour {
+
+    public Transform target;
+    private float range = 20f;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (target == null)
+            return;
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = lookRotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
--- Assets/Scripts/Walker/WalkerBehavior.cs
+++ Assets/Scripts/Walker/WalkerBehavior.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WalkerPlayerFollower : MonoBehaviour {
+public class WalkerBehavior : MonoBehaviour {
 
     public Transform target;
-    private int range = 30;
+    private int range = 20;
+    public static bool inRange = false;
+    public Transform body;
 
     // Use this for initialization
     void Start () {
@@ -21,20 +23,27 @@ public class WalkerPlayerFollower : MonoBehaviour {
                 var lookPos = target.position - transform.position;
                 lookPos.y = 0;
                 var rotation = Quaternion.LookRotation(lookPos);
-                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 10);
-                WalkerMovement.isWalking = false;
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 5);
+                inRange = true;
             }
             else
             {
                 var lookPos = target.position - transform.position;
                 var rotation = Quaternion.LookRotation(lookPos);
-                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 10);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 5);
             }
         }
         else
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.identity, Time.deltaTime * 5);
-            WalkerMovement.isWalking = true;
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.identity, Time.deltaTime * 5);
+            transform.rotation = Quaternion.Slerp(transform.rotation, body.rotation, Time.deltaTime * 5);
+            inRange = false;
         }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
